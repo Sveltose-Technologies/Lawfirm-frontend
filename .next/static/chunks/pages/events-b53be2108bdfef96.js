c45f2d49c1876520a0542f28c6cba8b81d637c5e
Sveltose-Technologiesgit config --global user.name sveltosehr@gmail.comgit config --global user.name Sveltose-Technologies <sveltosehr@gmail.com>
2026-01-03 18:48:04 +0530

    update code

--- .next/static/chunks/pages/events-b53be2108bdfef96.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[695],{3129:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/events",function(){return u(7777)}])}},function(n){n.O(0,[777,774,888,179],(function(){return _=3129,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
